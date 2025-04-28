ba103f5ac2df944d06a4b3b6cac926f4bede9894
RyanOngaro <68353478+RyanOngaro@users.noreply.github.com>
2025-04-28 14:42:16 -0400

    FPS task

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +19,8 @@ def main():
         screen.fill("black")    
         pygame.display.flip()
 
+        # locking framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
