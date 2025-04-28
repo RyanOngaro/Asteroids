55e94a60e31fae52e55af61759a098237c320978
RyanOngaro <68353478+RyanOngaro@users.noreply.github.com>
2025-04-28 15:24:08 -0400

    Draw Player lesson

--- main.py
+++ main.py
@@ -2,21 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")    
+        screen.fill("black")   
+        player.draw(screen)
         pygame.display.flip()
 
         # locking framerate to 60 fps
